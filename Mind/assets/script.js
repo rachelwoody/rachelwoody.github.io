a89ae2a8d376eb1266febfe75317dc0f7c8fcdee
Rachel Woody <rwoody@berkeley.edu>
2019-04-29 01:13:46 -0700

    Started final project

---
+++ Mind/assets/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $(".slider-area").slick({
+    dots: false,
+    infinite: true,
+    variableWidth: true,
+    arrows: true,
+    autoplay: true,
+
+  })
+
+
+
+})
